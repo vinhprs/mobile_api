845ba09bf209bbcdfa712bf1f72c59a62ed4f530
Nguyen Cao Hong Vinh <vinhnguyen19052002@gmail.com>
2023-11-13 14:18:05 +0000

    public get subjects

--- src/modules/subjects/controllers/subjects.controller.ts
+++ src/modules/subjects/controllers/subjects.controller.ts
@@ -7,12 +7,13 @@ import {
 import { SubjectsService } from '../providers';
 import { BaseApiResponse } from '../../../shared/dtos';
 import { SubjectOutput } from '../dto';
-
+import { Public } from '../../common';
 @Controller()
 export class SubjectsController {
   constructor(private readonly subjectService: SubjectsService) {}
 
   @Get()
+  @Public()
   @UseInterceptors(ClassSerializerInterceptor)
   async getSubjects(): Promise<BaseApiResponse<SubjectOutput[]>> {
     return this.subjectService.getSubjects();
